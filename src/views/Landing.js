a83f866d116f9c53db7a869f2aac6bbf60d58a34
Kevin Bryan Buenaseda <kevinbryanbuenaseda@gmail.com>
2022-12-01 01:29:40 +0800

    with github pages

--- src/views/Landing.js
+++ src/views/Landing.js
@@ -6,117 +6,180 @@ import { IconContext } from "react-icons";
 import { AiOutlineRight } from "react-icons/ai";
 
 function Landing() {
-  return (
-    <>
-      <div className=" md:mt-[88px] mt-[82px] bg-[#FEFCF0] lg:h-screen flex">
-        <div className=" w-[95%] mx-auto my-auto pb-[88px] ">
-          <div className="flex flex-col lg:flex-row justify-center">
-            <div className="text-5xl lg:w-[30%] my-auto font-normal">
-              <span className="text-[#DB723F] font-bold">Bull Vaulter</span>
-              <div className="text-[#753536]">Alena of the Isle of Green</div>
-            </div>
-            <div className="lg:w-[40%]">
-              <img
-                src="https://as1.ftcdn.net/v2/jpg/00/94/20/12/1000_F_94201267_qTtmC14hkFSRuEaMWK6sa3TrwQipodhU.jpg"
-                alt=""
-              />
-            </div>
-            <div className="text-5xl lg:w-[30%] my-auto text-right">
-              <span className="text-[#DB723F]">Barbara</span>{" "}
-              <span className="text-[#753536]">Tieken</span>
-            </div>
-          </div>
-          <div>
-            <div className="lg:text-5xl text-4xl text-center my-4">
-              Now Available
-            </div>
-            <a
-              href="https://www.amazon.com/Bull-Vaulter-Alena-Isle-Green-ebook/dp/B091THW56C/ref=cm_cr_arp_d_product_top?ie=UTF8"
-              target="_blank"
-              rel="noreferrer"
-            >
-              <div class="border-2 border-black rounded-lg bg-[#FEFCF0] text-md px-5 md:py-2.5 py-3.5 max-w-[250px] my-4 mx-auto">
-                <img
-                  className="pt-2 "
-                  src="https://jamesanstead.com/wp-content/uploads/2021/02/580b57fcd9996e24bc43c518-1-300x60.png"
-                  alt=""
-                />
-              </div>
-            </a>
-          </div>
-        </div>
-      </div>
-      <div>
-        <img className="" src={bullVauterImage} alt="" />
-      </div>
-      <div className=" bg-black">
-        <div className="text-white w-[95%] mx-auto py-20">
-          <div className="lg:text-7xl md:text-5xl text-4xl flex  justify-between pb-4 md:w-[80%] mx-auto">
-            <div className="my-auto">Barbara Tieken</div>
-            <div className=" md:min-w-[380px] min-w-[180px] max-w-[380px]">
-              <img
-                className=""
-                src="https://m.media-amazon.com/images/I/41wkWL1eiAL._SX450_.jpg"
-                alt=""
-              />
-            </div>
-          </div>
-          <div className="">
-            <div className="md:text-3xl sm:text-2xl text-lg flex flex-col text-justify md:w-[50%] w-[70%] mx-auto">
-              <div className="">
-                Barbara Tieken, author of Bull Vaulter, Alena of the Isle of
-                Green, has now returned to her childhood home in Texas where she
-                writes a weekly column for Historic Old Town Shiner in The
-                Shiner Gazette. Barbara has been described as “a friend of all
-                who fly, be it bird or spirit.” An avid environmentalist,
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div className="bg-[#FEFCF0] py-8">
-        <div className="w-[70%] mx-auto ">
-          <div>
-            <div className="lg:text-7xl text-4xl  py-6 uppercase text-black">
-              The US Review of Books
-            </div>
+	return (
+		<>
+			<div className=" md:mt-[88px] mt-[92px] bg-[#FEFCF0] lg:h-screen flex">
+				<div className=" w-[95%] mx-auto my-auto pb-[88px] ">
+					<div className="flex flex-col lg:flex-row justify-center">
+						<div className="text-5xl lg:w-[30%] my-auto font-normal">
+							<span className="text-[#DB723F] font-bold">
+								Bull Vaulter
+							</span>
+							<div className="text-[#753536]">
+								Alena of the
+								Isle of Green
+							</div>
+						</div>
+						<div className="lg:w-[40%]">
+							<img
+								src="https://as1.ftcdn.net/v2/jpg/00/94/20/12/1000_F_94201267_qTtmC14hkFSRuEaMWK6sa3TrwQipodhU.jpg"
+								alt=""
+							/>
+						</div>
+						<div className="text-5xl lg:w-[30%] my-auto text-right">
+							<span className="text-[#DB723F]">
+								Barbara
+							</span>{" "}
+							<span className="text-[#753536]">
+								Tieken
+							</span>
+						</div>
+					</div>
+					<div>
+						<div className="lg:text-5xl text-4xl text-center my-4">
+							Now Available
+						</div>
+						<a
+							href="https://www.amazon.com/Bull-Vaulter-Alena-Isle-Green-ebook/dp/B091THW56C/ref=cm_cr_arp_d_product_top?ie=UTF8"
+							target="_blank"
+							rel="noreferrer"
+						>
+							<div class="border-2 border-black rounded-lg bg-[#FEFCF0] text-md px-5 md:py-2.5 py-3.5 max-w-[250px] my-4 mx-auto">
+								<img
+									className="pt-2 "
+									src="https://jamesanstead.com/wp-content/uploads/2021/02/580b57fcd9996e24bc43c518-1-300x60.png"
+									alt=""
+								/>
+							</div>
+						</a>
+					</div>
+				</div>
+			</div>
+			<div>
+				<img
+					className=""
+					src={bullVauterImage}
+					alt=""
+				/>
+			</div>
+			<div className=" bg-black">
+				<div className="text-white w-[95%] mx-auto py-20">
+					<div className="lg:text-7xl md:text-5xl text-4xl flex  justify-between pb-4 md:w-[80%] mx-auto">
+						<div className="my-auto">
+							Barbara Tieken
+						</div>
+						<div className=" md:min-w-[380px] min-w-[180px] max-w-[380px]">
+							<img
+								className=""
+								src="https://m.media-amazon.com/images/I/41wkWL1eiAL._SX450_.jpg"
+								alt=""
+							/>
+						</div>
+					</div>
+					<div className="">
+						<div className="md:text-3xl sm:text-2xl text-lg flex flex-col text-justify md:w-[50%] w-[70%] mx-auto">
+							<div className="">
+								Barbara Tieken,
+								author of Bull
+								Vaulter, Alena
+								of the Isle of
+								Green, has now
+								returned to her
+								childhood home
+								in Texas where
+								she writes a
+								weekly column
+								for Historic Old
+								Town Shiner in
+								The Shiner
+								Gazette. Barbara
+								has been
+								described as “a
+								friend of all
+								who fly, be it
+								bird or spirit.”
+								An avid
+								environmentalist,
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div className="bg-[#FEFCF0] py-8">
+				<div className="w-[70%] mx-auto ">
+					<div>
+						<div className="lg:text-7xl text-4xl  py-6 uppercase text-black">
+							The US Review of Books
+						</div>
 
-            <div className="text-black text-lg lg:w-[50%] w-[80%]">
-              One horrible night in the ancient Peloponnesus, members of an
-              invading tribe double rape a fourteen-year-old girl. The attack
-              leaves her injured and pregnant. But Alena is tough beyond her
-              years. She is a bull vaulter, rigorously trained to execute
-              acrobatic feats on the back of a galloping bull—her matriarchal
-              society's symbol of strength and rebirth.
-              <Link to="reviews" className="text-xl my-4 text-[#753536] flex">
-                Read More{" "}
-                <IconContext.Provider value={{ size: "1.8em" }}>
-                  <AiOutlineRight />
-                </IconContext.Provider>
-              </Link>
-            </div>
-          </div>
-          <div>
-            <div className="lg:text-7xl text-4xl py-6 uppercase text-black">
-              Pacific Book Review
-            </div>
-            <div className="text-black font-light text-lg lg:w-[50%] w-[80%] ">
-              Set in 2100 B.C.E, Bull Vaulter: Alena of the Isle of Green is one
-              of those uncommon pieces of fiction literature which features
-              convincing insights built on meticulous research – while still
-              providing readers a rich and riveting narrative.
-              <Link to="reviews" className="text-xl text-[#753536] flex">
-                Read More{" "}
-                <IconContext.Provider value={{ size: "1.8em" }}>
-                  <AiOutlineRight />
-                </IconContext.Provider>
-              </Link>
-            </div>
-          </div>
-        </div>
-      </div>
-    </>
-  );
+						<div className="text-black text-lg lg:w-[50%] w-[80%]">
+							One horrible night in
+							the ancient
+							Peloponnesus, members of
+							an invading tribe double
+							rape a fourteen-year-old
+							girl. The attack leaves
+							her injured and
+							pregnant. But Alena is
+							tough beyond her years.
+							She is a bull vaulter,
+							rigorously trained to
+							execute acrobatic feats
+							on the back of a
+							galloping bull—her
+							matriarchal society's
+							symbol of strength and
+							rebirth.
+							<Link
+								to="reviews"
+								className="text-xl my-4 text-[#753536] flex"
+							>
+								Read More{" "}
+								<IconContext.Provider
+									value={{
+										size: "1.8em",
+									}}
+								>
+									<AiOutlineRight />
+								</IconContext.Provider>
+							</Link>
+						</div>
+					</div>
+					<div>
+						<div className="lg:text-7xl text-4xl py-6 uppercase text-black">
+							Pacific Book Review
+						</div>
+						<div className="text-black font-light text-lg lg:w-[50%] w-[80%] ">
+							Set in 2100 B.C.E, Bull
+							Vaulter: Alena of the
+							Isle of Green is one of
+							those uncommon pieces of
+							fiction literature which
+							features convincing
+							insights built on
+							meticulous research –
+							while still providing
+							readers a rich and
+							riveting narrative.
+							<Link
+								to="reviews"
+								className="text-xl text-[#753536] flex"
+							>
+								Read More{" "}
+								<IconContext.Provider
+									value={{
+										size: "1.8em",
+									}}
+								>
+									<AiOutlineRight />
+								</IconContext.Provider>
+							</Link>
+						</div>
+					</div>
+				</div>
+			</div>
+		</>
+	);
 }
 
 export default Landing;
--- src/views/NavBar.js
+++ src/views/NavBar.js
@@ -4,65 +4,71 @@ import React, { useState } from "react";
 import { motion, AnimatePresence } from "framer-motion";
 
 const NavBar = () => {
-  const [hamBurgetState, setHamBurgetState] = useState(false);
-  const [aboutState, setAboutState] = useState(false);
+	const [hamBurgetState, setHamBurgetState] = useState(false);
+	const [aboutState, setAboutState] = useState(false);
 
-  return (
-    <>
-      {" "}
-      <nav
-        className="bg-[#FEFCF0] px-4 sm:px-8 py-4 fixed top-0 w-full z-50 text-black"
-        id="navbar"
-      >
-        <div className=" flex flex-wrap justify-between items-center mx-auto">
-          <Link to="author" className="flex items-center">
-            <span className="md:text-5xl text-xl font-semibold whitespace-nowrap font-Montserrat ">
-              Barbara Tieken
-            </span>
-          </Link>
+	return (
+		<>
+			{" "}
+			<nav
+				className="bg-[#FEFCF0] px-4 sm:px-8 py-4 fixed top-0 w-full z-50 text-black"
+				id="navbar"
+			>
+				<div className=" flex flex-wrap justify-between items-center mx-auto">
+					<Link
+						to="author"
+						className="flex items-center"
+					>
+						<span className="md:text-5xl text-xl font-semibold whitespace-nowrap font-Montserrat ">
+							Barbara Tieken
+						</span>
+					</Link>
 
-          <div className="navLink  w-full flex md:w-auto" id="navbar-default">
-            <ul className="flex  p-4 md:space-x-15 space-x-4 mx-auto mt-0 text-md font-medium ">
-              <li>
-                <Link
-                  to="/"
-                  className="py-2  rounded md:bg-transparent md:p-0 md:hover:text-[#DB723F] "
-                  aria-current="page"
-                >
-                  Home
-                </Link>
-              </li>
+					<div
+						className="navLink  w-full flex md:w-auto "
+						id="navbar-default"
+					>
+						<ul className="flex  md:p-4 p-1 md:space-x-15 space-x-4  mx-auto mt-0 text-md font-medium ">
+							<li>
+								<Link
+									to="/"
+									className="py-2  rounded md:bg-transparent md:p-0 md:hover:text-[#DB723F] "
+									aria-current="page"
+								>
+									Home
+								</Link>
+							</li>
 
-              <li>
-                <Link
-                  to="author"
-                  className=" py-2 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-[#DB723F] md:p-0 "
-                >
-                  Author
-                </Link>
-              </li>
-              <li>
-                <Link
-                  to="book"
-                  className=" py-2 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-[#DB723F] md:p-0 "
-                >
-                  Book
-                </Link>
-              </li>
-              <li>
-                <Link
-                  to="reviews"
-                  className=" py-2  rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-[#DB723F] md:p-0 "
-                >
-                  Reviews
-                </Link>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </nav>
-    </>
-  );
+							<li>
+								<Link
+									to="author"
+									className=" py-2 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-[#DB723F] md:p-0 "
+								>
+									Author
+								</Link>
+							</li>
+							<li>
+								<Link
+									to="book"
+									className=" py-2 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-[#DB723F] md:p-0 "
+								>
+									Book
+								</Link>
+							</li>
+							<li>
+								<Link
+									to="reviews"
+									className=" py-2  rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-[#DB723F] md:p-0 "
+								>
+									Reviews
+								</Link>
+							</li>
+						</ul>
+					</div>
+				</div>
+			</nav>
+		</>
+	);
 };
 
 export default NavBar;
